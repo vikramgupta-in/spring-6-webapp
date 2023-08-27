4a3ea048f97b533f7539a84796d3b57e04ef7d52
VikramG <VikramG@boxer.co.za>
2023-08-27 23:56:13 +0200

    4- bootstrap-data

---
+++ src/main/java/guru/springframework/spring6webapp/domain/Publisher.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring6webapp.domain;
+
+public class Publisher {
+
+    private Long id;
+    private String publisherName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+}
